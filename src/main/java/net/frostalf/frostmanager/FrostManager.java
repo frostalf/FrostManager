c6f59e2d1b286642498ea58a9d396bab1f1ca7a7
frostalf <frostalf@gmail.com>
2014-01-07 05:08:14 -0800

    Cache Data.

--- src/main/java/net/frostalf/frostmanager/FrostManager.java
+++ src/main/java/net/frostalf/frostmanager/FrostManager.java
@@ -3,9 +3,11 @@ package net.frostalf.frostmanager;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import net.frostalf.frostmanager.listeners.FrostManagerListener;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -19,6 +21,9 @@ public class FrostManager extends JavaPlugin {
     private YamlConfiguration data;
     private ArrayList<String> pluginNames = new ArrayList<String>();
     private HashMap<Integer, PluginInfo> plugins = new HashMap<>();
+    private FrostManager plugin;
+    private PluginManager pm = plugin.getServer().getPluginManager();
+    
     
     @Override
     public void onEnable(){
@@ -31,7 +36,9 @@ public class FrostManager extends JavaPlugin {
     }
     
     public void cacheData(){
-        
+        for(String name : Arrays.asList(pm.getPlugins().toString())){
+            this.pluginNames.add(name);
+        }
     }
     
 }
